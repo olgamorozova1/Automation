f42284a3273984220c1d02099a0e0f8b97cd7ee8
olgamorozova1 <marozavavolha@gmail.com>
2020-10-15 21:20:30 +0300

    Completed task

---
+++ src/test/java/Constants/testGroups.java
@@ -0,0 +1,6 @@
+package Constants;
+
+public class testGroups {
+    public static final String CHECKIN = "checkintest";
+    public static final String FUNCTIONAL = "functest";
+}
--- src/test/java/testGroups/testGroups.java
+++
@@ -1,6 +0,0 @@
-package testGroups;
-
-public class testGroups {
-    public static final String GROUP1 = "checkintest";
-    public static final String GROUP2 = "functest";
-}
